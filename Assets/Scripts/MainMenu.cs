54e3a61103358110f0ea786d084d104f844adbc3
Mark Joshua Fajardo <mjt.fajardo@gmail.com>
2021-04-18 20:44:47 +0800

    feat: update 50%; add stop app til may 15

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject SubscriptionCover;
+
+    void Start()
+    {
+        var parameterDate = DateTime.ParseExact("05/15/2021", "MM/dd/yyyy", CultureInfo.InvariantCulture);
+        var todaysDate = DateTime.Today;
+
+        if (parameterDate <= todaysDate)
+        {
+            SubscriptionCover.SetActive(true);
+        }
+    }
+
     /**
      * Exit game.
      */
