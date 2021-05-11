28162c721a167fd9b0e4626c49836124f705ebf6
Mark Joshua Fajardo <mjt.fajardo@gmail.com>
2021-05-11 21:21:03 +0800

    fix: missing files

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
-        var parameterDate = DateTime.ParseExact("05/15/2021", "MM/dd/yyyy", CultureInfo.InvariantCulture);
+        var parameterDate = DateTime.ParseExact("05/22/2021", "MM/dd/yyyy", CultureInfo.InvariantCulture);
         var todaysDate = DateTime.Today;
 
         if (parameterDate <= todaysDate)
